2d99dbaac25365e4d59851cd017da3aa852a86e2
George McNinch <gmcninch@gmail.com>
2024-04-10 14:19:31 -0400

    asdf

--- bib/auto/teaching.el
+++ bib/auto/teaching.el
@@ -8,13 +8,18 @@
     "ballCourseAlgebraicErrorCorrecting2020"
     "beachyBlair"
     "ceccherini-silbersteinDiscreteHarmonicAnalysis2018"
+    "diaconisGeneralizationSpectralAnalysis1989"
     "fitzpatrickAdvancedCalculus2009"
+    "goldschmidtAlgebraicFunctionsProjective2003"
     "gutermanNitecki"
     "huffmanFundamentalsErrorCorrectingCodes2003"
     "jamesRepresentationsCharactersGroups2001"
     "macbethMechanicsProof"
     "milnej.s.GroupTheory"
+    "rijkeIntroductionHomotopyType"
+    "rosenNumberTheoryFunction2002"
     "serreLinearRepresentationsFinite1977"
+    "serreRationalPointsCurves2020"
     "spenceIntroductionAlgebraicCoding2002"
     "tsfasmanAlgebraicGeometricCodes2007"))
  '(or :bibtex :latex))
---
+++ course-posts/week12--formalization.md
@@ -0,0 +1,9 @@
+---
+title: |
+ Week12 -- Formalization of Math (Lean & Type theory)
+author: George McNinch  
+date: 2024-04-08
+---
+
+
+- [note from Monday [2024-04-08]](/course-contents/notes-formal-02.html)
